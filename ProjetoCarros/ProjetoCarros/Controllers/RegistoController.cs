3bb73b5718222da6bdcad87d491f15a426dca28a
samanta-bordallo <bordallo.samanta@gmail.com>
2023-11-02 09:15:22 +0000

    add project

---
+++ ProjetoCarros/ProjetoCarros/Controllers/RegistoController.cs
@@ -0,0 +1,105 @@
+﻿using ProjetoCarros.Data;
+using ProjetoCarros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoCarros.Controllers
+{
+    public class RegistoController : Controller
+    {
+        readonly private ApplicationDBContext _db;
+
+
+        public RegistoController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CarrosModel> registos = _db.Registo;
+
+
+            return View(registos);
+        }
+
+        [HttpGet]
+        public IActionResult NovoRegisto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NovoRegisto(CarrosModel registo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Registo.Add(registo);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registo);
+        }
+
+        [HttpGet]
+        public IActionResult EditarRegisto(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            CarrosModel registo = _db.Registo.FirstOrDefault(x => x.Id == id);
+            if (registo == null)
+            {
+                return NotFound(id);
+            }
+
+            return View(registo);
+        }
+        [HttpPost]
+        public IActionResult EditarRegisto(CarrosModel registo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Registo.Update(registo);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registo);
+
+
+        }
+
+
+        [HttpGet]
+        public IActionResult EliminarRegisto(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            CarrosModel registo = _db.Registo.FirstOrDefault(x => x.Id == id);
+            if (registo == null)
+            {
+                return NotFound(id);
+            }
+
+            return View(registo);
+        }
+
+        [HttpPost]
+        public IActionResult EliminarRegisto(CarrosModel registo)
+        {
+            if (registo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Registo.Remove(registo);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
